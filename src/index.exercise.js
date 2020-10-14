36e96b18ea917a32dfd7dee72e19f15b717b04b8
Sibasish Mohanty <smsibasish@gmail.com>
2020-10-14 17:23:42 +0530

    reach dialog

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,17 +1,47 @@
+import '@reach/dialog/styles.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
+import VisuallyHidden from '@reach/visually-hidden'
+import {Dialog} from '@reach/dialog'
 
 import {Logo} from './components/logo'
 
 function App() {
+  const [modalType, setModalType] = React.useState('none')
+
+  const closeModal = () => setModalType('none')
+
   return (
     <>
       <Logo />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert('Trying to login...')}>Login</button>
-        <button onClick={() => alert('Trying to register...')}>Register</button>
+        <button onClick={() => setModalType('login')}>Login</button>
+        <button onClick={() => setModalType('register')}>Register</button>
       </div>
+      <Dialog
+        aria-label="Login Modal"
+        isOpen={modalType === 'login'}
+        onDismiss={closeModal}
+      >
+        <button className="close-button" onClick={closeModal}>
+          <VisuallyHidden>Close</VisuallyHidden>
+          <span aria-hidden>X Close</span>
+        </button>
+        <p>Login</p>
+      </Dialog>
+      <Dialog
+        aria-label="Resgistration Modal"
+        isOpen={modalType === 'register'}
+        onDismiss={closeModal}
+      >
+        <button className="close-button" onClick={closeModal}>
+          <VisuallyHidden>Close</VisuallyHidden>
+          <span aria-hidden>X Close</span>
+        </button>
+        <p>Register</p>
+      </Dialog>
     </>
   )
 }
